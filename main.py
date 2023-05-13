c1b1d8d5b4a628e92bffd126509859ac52d555ca
JairAssisDev <jairv141@gmail.com>
2023-05-12 23:18:09 -0300

    atlz

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+import tensorflow as tf
+
+
+image_width = 160
+image_height = 160
+image_color_channel = 3
+image_color_channel_size = 255
+image_size = (image_width, image_height)
+image_shape = image_size + (image_color_channel,)
+
+batch_size = 32
+epochs = 20
+learning_rate = 0.0001
+
+
+model = tf.keras.models.load_model('model')
+
+
+def predict(image_file):
+
+    image = tf.keras.preprocessing.image.load_img(image_file, target_size = image_size)
+    image = tf.keras.preprocessing.image.img_to_array(image)
+    image = tf.expand_dims(image, 0)
+
+    prediction = model.predict(image)[0][0]
+
+    print('Prediction: {0} | {1}'.format(prediction, ('cat' if prediction < 0.5 else 'dog')))
+
+
+
+def predict_url(image_fname, image_origin):
+
+    image_file = tf.keras.utils.get_file(image_fname, origin = image_origin)
+    return predict(image_file)
+
+
+predict_url()
+x=0
+predict(f'img{x+1}.jpg')
