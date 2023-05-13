eb13e2300698d342fa1e1eaa79d07ff13b15d2c1
JairAssisDev <jairv141@gmail.com>
2023-05-13 10:31:17 -0300

    atlz

--- app.py
+++ app.py
@@ -1,49 +1,11 @@
-import os
-from flask import Flask, request, jsonify
-import tensorflow as tf
+from flask import Flask, jsonify
+from main import teste
 
-app = Flask(__name__)
+app= Flask(__name__)
 
-# carrega o modelo
-model = tf.keras.models.load_model('model')
+@app.route('/teste', methods=["GET"])
+def get_test():
+    return jsonify(teste('img2.jpg'))
 
-# configuração das dimensões da imagem
-image_width = 160
-image_height = 160
-image_color_channel = 3
-image_size = (image_width, image_height)
-image_shape = image_size + (image_color_channel,)
-
-# função para fazer a previsão do modelo
-def predict(image_file):
-    image = tf.keras.preprocessing.image.load_img(image_file, target_size=image_size)
-    image = tf.keras.preprocessing.image.img_to_array(image)
-    image = tf.expand_dims(image, 0)
-    prediction = model.predict(image)[0][0]
-    return {'prediction': prediction, 'class': 'cat' if prediction < 0.5 else 'dog'}
-
-# rota para fazer a previsão
-@app.route('/predict', methods=['POST'])
-def predict_route():
-    if 'image' not in request.files:
-        print(request.files)
-        return jsonify({'error': 'no image file'})
-        
-    file = request.files['image']
-    if file.filename == '':
-        return jsonify({'error': 'no image file'})
-    image_file = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-    file.save(image_file)
-    prediction = predict(image_file)
-    return jsonify(prediction)
-
-# rota para verificar se a API está funcionando
-@app.route('/health')
-def health_check():
-    return 'API está funcionando'
-
-if __name__ == '__main__':
-    app.config['UPLOAD_FOLDER'] = './uploads'
-    app.run(debug=True)
-
+app.run()
 
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import json
 import os
 import tensorflow as tf
 
+import json
+import numpy as np
 
 image_width = 160
 image_height = 160
@@ -13,25 +16,26 @@ batch_size = 32
 epochs = 20
 learning_rate = 0.0001
 
-#dsdsd
 model = tf.keras.models.load_model('model')
 
 
-def predict(image_file):
+def json_serializable(obj):
+    if isinstance(obj, np.float32):
+        return float(obj)
+    elif isinstance(obj, np.ndarray):
+        return obj.tolist()
+    else:
+        raise TypeError(f'Object of type {obj.__class__.__name__} is not JSON serializable')
 
-    image = tf.keras.preprocessing.image.load_img(image_file, target_size = image_size)
+def teste(x):
+    
+    image = tf.keras.preprocessing.image.load_img(x, target_size=image_size)
     image = tf.keras.preprocessing.image.img_to_array(image)
     image = tf.expand_dims(image, 0)
 
     prediction = model.predict(image)[0][0]
 
-    print('Prediction: {0} | {1}'.format(prediction, ('cat' if prediction < 0.5 else 'dog')))
+    result = {'prediction': prediction, 'class': 'cat' if prediction < 0.5 else 'dog'}
 
+    return json.dumps(result, default=json_serializable)
 
-
-
-
-
-
-x=1
-predict(f'img{x+1}.jpg')
